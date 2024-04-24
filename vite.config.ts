c564e9889051c0149c66722cf29ceb356e8115d2
capitanflex <danilov.zo@yandex.ru>
2024-04-24 23:03:36 +0300

    hotfix & build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import aliases from 'vite-aliases';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+
   },
+  base: '/ServerStatus/dist/'
 });
