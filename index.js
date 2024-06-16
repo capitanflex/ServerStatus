90d64327d2c8ced7c593479d166eb70fe8a8e2b9
capitanflex <danilov.zo@yandex.ru>
2024-06-16 23:08:02 +0300

    change port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the application.' });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 55873;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 
